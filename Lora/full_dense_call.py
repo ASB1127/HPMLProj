de46e8d0fabfe7502ba2f7ab7a7fce2ff4aff8fc
amit bal <amitsinghbal27@gmail.com>
2025-12-01 18:26:44 -0500

    added full_dense training as a tester

---
+++ Lora/full_dense_call.py
@@ -0,0 +1,9 @@
+from lora_config.lora import lora_run, MemoryPeakPerEpochCallback
+from forward_pass.profile_forward import profiler_forward as profile_forward
+
+num_train_epochs, learning_rate = 5, 2e-4
+ranks = [4,8,16,64,128]
+for rank in ranks:
+    runner = lora_run(num_train_epochs, rank, learning_rate)
+    runner.run()
+    profile_forward(rank).runner()   
--- Lora/print_model.py
+++ Lora/print_model.py
@@ -6,3 +6,5 @@ model = AutoModel.from_pretrained("distilbert-base-uncased")
 for name, param in model.named_parameters():
     print(f"{name:50s}  shape={list(param.shape)}  params={param.numel()}")
 
+
+print(model)
