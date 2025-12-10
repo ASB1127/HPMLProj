458cca004f729b871862f4df3d115c693ac5a830
amit bal <amitsinghbal27@gmail.com>
2025-12-09 22:24:46 -0500

    fixed in assert in customAdam

--- Lora+TopR/lora_config/custom_adamw_topr.py
+++ Lora+TopR/lora_config/custom_adamw_topr.py
@@ -4,7 +4,7 @@ from torch.optim import AdamW
 class TopRAdamW(AdamW):
     def __init__(self, params, lr=1e-3, top_r=1.0, **kwargs):
         super().__init__(params, lr=lr, **kwargs)
-        assert 0 < top_r <= 1.0, 
+        assert 0 < top_r <= 1.0
         self.top_r = top_r
 
     @torch.no_grad()
