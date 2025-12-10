bd535fde836f47484541aa9aa7f06119b4646b78
root <amitsinghbal27@gmail.com>
2025-12-10 03:40:50 +0000

    did a run for rank 4 for topr masking

--- Lora+TopR/lora_call.py
+++ Lora+TopR/lora_call.py
@@ -1,7 +1,7 @@
 from lora_config.lora import lora_run, MemoryPeakPerEpochCallback
 
 num_train_epochs, learning_rate = 5, 2e-4
-ranks = [4,8,16,64,128]
+ranks = [8]
 for rank in ranks:
     runner = lora_run(num_train_epochs, rank, learning_rate)
     runner.run()
